e232d85ce4dead9ed6a48ab391c71de6226ad74b
Jerry Turcios <jerryturcios08@gmail.com>
2020-06-06 16:53:02 -0400

    Add sign up page

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import styles from "./SignUp.module.scss";
 
 export default function SignUp() {
+  const [emailInput, setEmailInput] = useState("");
+
+  function handleEmailInputChange(e) {
+    setEmailInput(e.target.value);
+  }
+
+  function handleFormSubmission(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className={styles.signUp}>
-      <h1>Sign Up</h1>
+      <Link className={styles.backButton} to="/login">
+        <i className="fas fa-chevron-left fa-2x"></i>
+      </Link>
+      <div className={styles.container}>
+        <form className={styles.formContainer} onSubmit={handleFormSubmission}>
+          <h1>What's your email?</h1>
+          <p>
+            Welcome to Mercoma. Connect with doctors and identify possible
+            tumors with a snapshot.
+          </p>
+          <div className={styles.inputContainer}>
+            <input
+              onChange={handleEmailInputChange}
+              placeholder="Email"
+              type="email"
+              value={emailInput}
+              required
+            />
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
