b04a56540369c59a2dac7ea1cd8486fe405f9463
Jerry Turcios <jerryturcios08@gmail.com>
2020-06-06 12:08:49 -0400

    Fix photo management for the modal

--- src/views/Photos/PhotoModal.js
+++ src/views/Photos/PhotoModal.js
@@ -5,25 +5,30 @@ import styles from "./PhotoModal.module.scss";
 
 function PhotoModal(props) {
   const [isFileAdded, setIsFileAdded] = useState(false);
-  const [imageFile, setImageFile] = useState(null);
-  let imageInput = useRef(null);
+  const [image, setImage] = useState(null);
+  let imageInputRef = useRef(null);
 
   function handleCloseButtonClicked() {
     props.closeModal();
   }
 
-  function handleImageChange() {
-    const files = imageInput.files;
+  function handleImageChange(e) {
+    const files = imageInputRef.files;
     const image = files[0];
+    const reader = new FileReader();
 
+    reader.onload = (e) => {
+      setImage(e.target.result);
+    };
+
+    reader.readAsDataURL(image);
     setIsFileAdded(true);
-    setImageFile(image);
   }
 
   function handleFormSubmit(e) {
     e.preventDefault();
 
-    const files = imageInput.files;
+    const files = imageInputRef.files;
   }
 
   return (
@@ -43,27 +48,11 @@ function PhotoModal(props) {
             {isFileAdded ? (
               <>
                 <p className="image is-4by3">
-                  <img src={imageFile} alt="Placeholder" />
+                  <img src={image} alt="Uploaded skin sample" />
                 </p>
-                <p className={styles.inputLabel}>Choose a different image</p>
-                <input
-                  className={styles.inputfile}
-                  type="file"
-                  name="file"
-                  id="file"
-                  accept="image/*"
-                  capture="camera"
-                  onChange={handleImageChange}
-                  ref={(e) => {
-                    imageInput = e;
-                  }}
-                />
-                <label htmlFor="file">
-                  <i className="fas fa-camera fa-2x" />
-                </label>
               </>
             ) : (
-              <>
+              <div id={styles.emptyPhotoContent}>
                 <p className={styles.inputLabel}>An image has not been added</p>
                 <input
                   className={styles.inputfile}
@@ -74,13 +63,13 @@ function PhotoModal(props) {
                   capture="camera"
                   onChange={handleImageChange}
                   ref={(e) => {
-                    imageInput = e;
+                    imageInputRef = e;
                   }}
                 />
                 <label htmlFor="file">
                   <i className="fas fa-camera fa-2x" />
                 </label>
-              </>
+              </div>
             )}
           </section>
           <footer className="modal-card-foot">
