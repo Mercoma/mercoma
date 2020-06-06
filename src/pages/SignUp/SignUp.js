4a0bc437adb2e37ee1f61d5f5cdcb6255fe76aa8
Jerry Turcios <jerryturcios08@gmail.com>
2020-06-06 17:36:43 -0400

    Add HTTP requests to the sign up page

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,17 +1,73 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { BASE_URL } from "../../constants";
+
 import styles from "./SignUp.module.scss";
 
-export default function SignUp() {
+export default function SignUp(props) {
+  const [firstNameInput, setFirstNameInput] = useState("");
+  const [lastNameInput, setLastNameInput] = useState("");
   const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const [confirmPasswordInput, setConfirmPasswordInput] = useState("");
+  const [isPasswordErrorVisible, setIsPasswordErrorVisible] = useState(false);
+  const [isRequestErrorVisible, setIsRequestErrorVisible] = useState(false);
+
+  function handleFirstNameInputChange(e) {
+    setFirstNameInput(e.target.value);
+    hideErrorStatusMessages();
+  }
+
+  function handleLastNameInputChange(e) {
+    setLastNameInput(e.target.value);
+    hideErrorStatusMessages();
+  }
 
   function handleEmailInputChange(e) {
     setEmailInput(e.target.value);
+    hideErrorStatusMessages();
+  }
+
+  function handlePasswordInputChange(e) {
+    setPasswordInput(e.target.value);
+    hideErrorStatusMessages();
+  }
+
+  function handleConfirmPasswordInputChange(e) {
+    setConfirmPasswordInput(e.target.value);
+    hideErrorStatusMessages();
+  }
+
+  function hideErrorStatusMessages() {
+    setIsPasswordErrorVisible(false);
+    setIsRequestErrorVisible(false);
   }
 
   function handleFormSubmission(e) {
     e.preventDefault();
+
+    const data = {
+      email: emailInput,
+      password: passwordInput,
+      firstname: firstNameInput,
+      lastname: lastNameInput,
+    };
+
+    if (passwordInput === confirmPasswordInput) {
+      axios
+        .post(`${BASE_URL}/users/signup`, data)
+        .then((res) => {
+          // store token
+          props.history.push("/login")
+        })
+        .catch(() => {
+          setIsRequestErrorVisible(true);
+        });
+    } else {
+      setIsPasswordErrorVisible(true);
+    }
   }
 
   return (
@@ -28,6 +84,24 @@ export default function SignUp() {
           </p>
           <div className={styles.inputContainer}>
             <input
+              onChange={handleFirstNameInputChange}
+              placeholder="First name"
+              type="text"
+              value={firstNameInput}
+              required
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <input
+              onChange={handleLastNameInputChange}
+              placeholder="Last name"
+              type="text"
+              value={lastNameInput}
+              required
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <input
               onChange={handleEmailInputChange}
               placeholder="Email"
               type="email"
@@ -35,8 +109,39 @@ export default function SignUp() {
               required
             />
           </div>
+          <div className={styles.inputContainer}>
+            <input
+              onChange={handlePasswordInputChange}
+              placeholder="Password"
+              type="password"
+              value={passwordInput}
+              required
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <input
+              onChange={handleConfirmPasswordInputChange}
+              placeholder="Confirm password"
+              type="password"
+              value={confirmPasswordInput}
+              required
+            />
+          </div>
           <button>Submit</button>
         </form>
+        {/* Status messages */}
+        {isPasswordErrorVisible && (
+          <div class={`notification is-danger ${styles.statusMessage}`}>
+            <button class="delete" onClick={hideErrorStatusMessages}></button>
+            Error! The passwords do no match.
+          </div>
+        )}
+        {isRequestErrorVisible && (
+          <div class={`notification is-danger ${styles.statusMessage}`}>
+            <button class="delete" onClick={hideErrorStatusMessages}></button>
+            An error was encountered! Please try again.
+          </div>
+        )}
       </div>
     </div>
   );
