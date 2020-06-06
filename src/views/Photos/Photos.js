326a0dc97bad73ae1ea1df5d52789e26de0fa03d
Jerry Turcios <jerryturcios08@gmail.com>
2020-06-06 02:45:35 -0400

    Add classnames for the modal

--- src/views/Photos/Photos.js
+++ src/views/Photos/Photos.js
@@ -1,14 +1,15 @@
+import classnames from "classnames";
 import React, { useEffect, useState } from "react";
 
 import Sidebar from "../../components/Sidebar";
 import { user1 } from "../../data/user1";
 import PhotoCard from "./PhotoCard";
-import PhotoModal from './PhotoModal';
 
 import styles from "./Photos.module.scss";
 
 export default function Photos() {
-  const [isModalVisible, setIsModalVisible] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  let classes = classnames("is-active", { active: isModalVisible });
 
   useEffect(() => {
     document.title = "Mercoma - Photos";
@@ -16,14 +17,32 @@ export default function Photos() {
 
   function handlePlacementCardClicked() {
     setIsModalVisible(true);
-    console.log("WEEEE");
   }
 
   return (
     <div className={styles.photos}>
-      {isModalVisible && (
-        <PhotoModal />
-      )}
+      <div className={`modal ${classes}`}>
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Add photo</p>
+            <button className="delete" aria-label="close"></button>
+          </header>
+          <section className="modal-card-body">
+            {/* Add photo and needed controls */}
+            <p class="image is-4by3">
+              <img
+                src="https://bulma.io/images/placeholders/1280x960.png"
+                alt="Placeholder"
+              />
+            </p>
+          </section>
+          <footer className="modal-card-foot">
+            <button className="button is-success">Submit</button>
+            <button className="button">Cancel</button>
+          </footer>
+        </div>
+      </div>
       <Sidebar page="photos" />
       <section className={styles.photosContent}>
         <h1>Photos</h1>
