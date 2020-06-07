5dbc79ef1d44870ff154d46b76dcbf3695124e5e
Jerry Turcios <jerryturcios08@gmail.com>
2020-06-07 00:50:18 -0400

    Add profile page

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,7 @@ import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import NotFound from "./pages/NotFound";
 import Photos from './pages/Photos';
+import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import PrivateRoute from './routes/PrivateRoute';
 
@@ -15,6 +16,7 @@ export default function AppRouter() {
         <Route exact path="/login" component={Login} />
         <Route path="/signUp" component={SignUp} />
         <PrivateRoute path="/dashboard/photos" component={Photos} />
+        <PrivateRoute path="/dashboard/profile" component={Profile} />
         <PrivateRoute path="/dashboard" component={Dashboard} />
         <PrivateRoute component={NotFound} />
       </Switch>
