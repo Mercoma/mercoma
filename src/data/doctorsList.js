5abfef71fd5828f7aed27a22dd3f1f4ae12b6058
Jerry Turcios <jerryturcios08@gmail.com>
2020-06-07 11:44:39 -0400

    Add static doctor data

---
+++ src/data/doctorsList.js
@@ -0,0 +1,30 @@
+import edHopeImage from "../assets/doctors/edhope.jpeg";
+import mikeImage from "../assets/doctors/mike.jpg";
+import rujutaImage from '../assets/doctors/rujuta.jpg';
+
+export const doctors = [
+  {
+    image: mikeImage,
+    fullName: "Dr. Mikhail Varshavski",
+    title: "Board Certified FM Physician NYC",
+    summary:
+      "Dr. Mikhail Varshavski, better known as “Dr. Mike,” is a primary care physician at Chatham Family Medicine. He believes in preventive care and in educating individuals on how small lifestyle changes can make a big impact on quality of life as well as health outcomes.",
+    contact: "linkedin.com/in/dr-mike-028a49199",
+  },
+  {
+    image: edHopeImage,
+    fullName: "Dr. Ed Hope",
+    title: "Your friendly neighbourhood Junior Doctor",
+    summary:
+      "I'm a junior doctor working in the NHS with an interest in acute medicine and medical education.",
+    contact: "linkedin.com/in/ed-hope-97988425",
+  },
+  {
+    image: rujutaImage,
+    fullName: "Dr. Rujuta Saksena",
+    title: "Hematology Oncologist, Medical Oncologist",
+    summary:
+      "Dr. Saksena is a dynamic, clinically oriented, hematologist/oncologist with several years of experience in the treatment of various cancers and blood disorders. She is triple board-certified in oncology, hematology and internal medicine.",
+    contact: "saksenarujuta@gmail.com"
+  },
+];
