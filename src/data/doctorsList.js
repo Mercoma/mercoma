7340cf4afd2ca298ffd9ff53dd3913898d20d8b9
Jerry Turcios <jerryturcios08@gmail.com>
2020-06-07 12:10:45 -0400

    Add extra doctors to the sample list

--- src/data/doctorsList.js
+++ src/data/doctorsList.js
@@ -1,9 +1,15 @@
+import earlyImage from "../assets/doctors/early.jpg";
 import edHopeImage from "../assets/doctors/edhope.jpeg";
+import farberImage from "../assets/doctors/farber.jpg";
+import hannaImage from "../assets/doctors/hanna.jpg";
+import markowskiImage from "../assets/doctors/markowski.jpg";
 import mikeImage from "../assets/doctors/mike.jpg";
-import rujutaImage from '../assets/doctors/rujuta.jpg';
+import mohamadImage from "../assets/doctors/mohamad.jpg";
+import rujutaImage from "../assets/doctors/rujuta.jpg";
 
 export const doctors = [
   {
+    id: 1,
     image: mikeImage,
     fullName: "Dr. Mikhail Varshavski",
     title: "Board Certified FM Physician NYC",
@@ -12,6 +18,7 @@ export const doctors = [
     contact: "linkedin.com/in/dr-mike-028a49199",
   },
   {
+    id: 2,
     image: edHopeImage,
     fullName: "Dr. Ed Hope",
     title: "Your friendly neighbourhood Junior Doctor",
@@ -20,11 +27,57 @@ export const doctors = [
     contact: "linkedin.com/in/ed-hope-97988425",
   },
   {
+    id: 3,
     image: rujutaImage,
     fullName: "Dr. Rujuta Saksena",
     title: "Hematology Oncologist, Medical Oncologist",
     summary:
       "Dr. Saksena is a dynamic, clinically oriented, hematologist/oncologist with several years of experience in the treatment of various cancers and blood disorders. She is triple board-certified in oncology, hematology and internal medicine.",
-    contact: "saksenarujuta@gmail.com"
+    contact: "saksenarujuta@gmail.com",
+  },
+  {
+    id: 4,
+    image: markowskiImage,
+    fullName: "Dr. Paul Markowski",
+    title: "Medical oncologist in New Jersey",
+    summary:
+      "Dr Markowski is a medical doctor who focuses on hematology oncology. He practices medicine at the Atlantic Medical Group Medical Diagnostic Associates Carol G Simon Cancer Center.",
+    contact: "markowskipaul@gmail.com",
+  },
+  {
+    id: 5,
+    image: farberImage,
+    fullName: "Dr. Charles M Farber",
+    title: "Hematologist in Morristown, New Jersey",
+    summary:
+      "Dr. Charles Farber is a board-certified, fellowship-trained oncologist at Atlantic Hematology Oncology located at Morristown Medical Center’s Carol G. Simon Cancer Center. He maintains a broad-based practice, yet focuses on benign and malignant hematology.",
+    contact: "farbercharles@gmail.com",
+  },
+  {
+    id: 6,
+    image: hannaImage,
+    fullName: "Dr. Ruby Hanna",
+    title: "Medical Oncologist",
+    summary:
+      "Dr Hanna is a medical oncologist who specializes in internal medicine and hematology. She currently works Denville, New Jersey at the Morristown Medical Center",
+    contact: "hannaruby@gmail.com",
+  },
+  {
+    id: 7,
+    image: earlyImage,
+    fullName: "Dr. Ellen Marie Early",
+    title: "Medical oncologist for the Atlantic Medical Group",
+    summary:
+      "An expert in cancer care, Dr. Ellen Early brings broad-based knowledge and expertise to Atlantic Medical Group. She joins Atlantic Hematology Oncology, a growing team that is housed at Carol G. Simon Cancer Center at Morristown Medical Center.",
+    contact: "earlymellen@gmail.com",
+  },
+  {
+    id: 8,
+    image: mohamadImage,
+    fullName: "Dr. Mohamad Cherry",
+    title: "Hematologist for the Morristown Medical Center",
+    summary:
+      "Triple board-certified in internal medicine, hematology and medical oncology, Dr. Mohamad Cherry is medical director of Hematology at Atlantic Health System. He joins Atlantic Hematology Oncology from the University of Oklahoma Health Sciences Center's Stephenson Cancer Center, bringing years of clinical knowledge and expertise.",
+    contact: "drcherrymohamad@gmail.com",
   },
 ];
